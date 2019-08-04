4dcb77941fe5fe0ef26f42544428ecff39d5d4cc
Andrea Simone Costa <andrysimo1997@gmail.com>
2019-08-04 21:19:18 +0200

    enhance utilities

--- src/utilities.js
+++ src/utilities.js
@@ -14,6 +14,10 @@ function isAsyncGenerator(fn) {
   return isFunction(fn) && fn instanceof asyncGenerator.constructor
 }
 
+const COMPUTE_RESULT_FN_KEY = Symbol();
+const COMPUTE_RESULT_FN_VALUE = Symbol();
+
 export { isAsyncGenerator };
 export { isGenerator };
 export { isFunction };
+export { COMPUTE_RESULT_FN_KEY, COMPUTE_RESULT_FN_VALUE };
