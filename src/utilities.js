1f9aee58e3ea91839eda9721f8c258905c0b81e0
Andrea Simone Costa <andrysimo1997@gmail.com>
2019-08-04 20:24:29 +0200

    add utilities file

---
+++ src/utilities.js
@@ -0,0 +1,19 @@
+function isFunction(fn) {
+  return typeof fn === "function";
+}
+
+function* generator() { }
+
+function isGenerator(fn) {
+  return isFunction(fn) && fn instanceof generator.constructor
+}
+
+async function* asyncGenerator() { }
+
+function isAsyncGenerator(fn) {
+  return isFunction(fn) && fn instanceof asyncGenerator.constructor
+}
+
+export { isAsyncGenerator };
+export { isGenerator };
+export { isFunction };
